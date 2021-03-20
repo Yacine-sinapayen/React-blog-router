1c4c78e6b7fffba8b945e16fb90313de751ef880
Yacine-sinapayen <yacine.sinapayen@hotmail.fr>
2021-03-20 18:51:47 +0100

    JSX et styles des composants

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ function Header() {
     <header className="header">
       <nav>
         <ul>
-          <li><a href="/" className="header__link">Acceuil</a></li>
+          <li><a href="/" className="header__link header__link--selected">Accueil</a></li>
           <li><a href="/angular" className="header__link">Angular</a></li>
           <li><a href="/react" className="header__link">React</a></li>
           <li><a href="/oclock" className="header__link">O'clock</a></li>
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -4,7 +4,7 @@ function Post() {
   return (
     <li className="post">
       <article>
-        <h2 className="post__title"> Titre du post </h2> 
+        <h2 className="post__title"> Titre du post </h2>
         <div className="post__category"> Category</div>
         <p className="post__content"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, ipsum labore amet quisquam excepturi dolores soluta alias quis ducimus rem sequi, corrupti incidunt laboriosam dicta dignissimos vero velit! Eveniet, iusto. </p>
       </article>
