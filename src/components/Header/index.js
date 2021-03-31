2872122ef194c9272102617666de39225671c22a
Yacine-sinapayen <yacine.sinapayen@hotmail.fr>
2021-03-31 11:00:20 +0200

    mise en place du router

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {link} from 'react-router-dom';
 import './style.scss';
 
 function Header({ categories }) {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // == Import : npm
 import React from 'react';
 import { render } from 'react-dom';
-
+import { BrowserRouter } from 'react-router-dom';
 // == Import : local
 // Composants
 import Blog from 'src/components/Blog';
@@ -9,7 +9,12 @@ import Blog from 'src/components/Blog';
 // == Render
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
-const rootReactElement = <Blog />;
+
+const rootReactElement = (
+  <BrowserRouter>
+    <Blog />;
+  </BrowserRouter>
+);
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
 const target = document.getElementById('root');
 // 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
