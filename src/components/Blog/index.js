a0769abb7587e556501cc507ee03aa0ad4943974
Yacine-sinapayen <yacine.sinapayen@hotmail.fr>
2021-04-01 14:44:28 +0200

    mise en place du composant Redirect

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Header from 'src/components/Header';
 import Posts from 'src/components/Posts';
@@ -32,6 +32,7 @@ function Blog() {
       <Header categories={categoriesData} />
       <Switch>
         {routes}
+        <Redirect from="/jquery" to="/autre" />
         <Route>
           <NotFound />
         </Route>
